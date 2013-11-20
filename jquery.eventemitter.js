940186fd011982612cd65ac9b130536eba80d36f
Corey Ballou <corey@coreyballou.com>
2013-11-19 22:02:22 -0500

    Adding support for multiple events per on/off call.

--- jquery.eventemitter.js
+++ jquery.eventemitter.js
@@ -8,18 +8,29 @@
     $.eventEmitter = function() {
         this._callbacks = {};
 
-        this.on = function(name, callback) {
-            if (!this._callbacks[name]) {
-                this._callbacks[name] = $.Callbacks('once unique memory');
+        this.on = function(names, callback) {
+            var names = names.split(' '),
+                l = names.length,
+                i;
+                
+            for (i = 0; i < l; i++) {
+                if (!this._callbacks[names[i]]) {
+                    this._callbacks[names[i]] = $.Callbacks('once unique memory');
+                }
+                this._callbacks[names[i]].add(callback);
             }
-            this._callbacks[name].add(callback);
         };
 
-        this.off = function(name, callback) {
-            if (!this._callbacks[name]) {
-                return;
+        this.off = function(names, callback) {
+            var names = names.split(' '),
+                l = names.length,
+                i;
+                
+            for (i = 0; i < l; i++) {
+                if (this._callbacks[names[i]]) {
+                    this._callbacks[names[i]].remove(callback);
+                }
             }
-            this._callbacks[name].remove(callback);
         };
 
         this.trigger = function(name) {
